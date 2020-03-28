ffc05c1cd3d73bdec522db41b779f8b2d766a322
Mark Hemmings <mark@mhemmings.com>
2020-03-28 18:22:20 +0000

    Twitter cards

--- html.go
+++ html.go
@@ -21,70 +21,74 @@ var tpl = `
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta property="og:url" content="{{.ImageURL}}" />
-    <meta property="og:image" content="{{.ImageURL}}" />
+    <meta property="og:url" content="https://fournaan.com{{.ImageURL}}" />
+    <meta property="og:image" content="https://fournaan.com{{.ImageURL}}" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@mhemmings" />
+    <meta name="twitter:title" content="Four naan, Jeremy?" />
+    <meta name="twitter:image" content="https://fournaan.com{{.ImageURL}}" />
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Four naan, Jeremy?</title>
     <style>
-   		html {
-			  position: relative;
-			  min-height: 100%;
-			}
-			body {
-			  margin-bottom: 95px; /* Margin bottom by footer height */
-    		background: #f8f9fa;
-    	}
-			.container{
-				// padding-top: 25px;
-				text-align: center;
-			  width: auto;
-			  max-width: 680px;
-			  padding: 25px 15px;
-			}
-			img {
-				max-width: 100%;
-				border-radius: 15px;
-			}
-			.input-group {
-				margin-top: 25px;
-			}
-			.footer {
-			  position: absolute;
-			  bottom: 0;
-			  width: 100%;
-			  height: 40px; /* Set the fixed height of the footer here */
-			  line-height: 40px; /* Vertically center the text there */
-			  background-color: #ffffff;
-			  text-align: center;
-			  font-size: 10pt;
-			}
-		</style>
+      html {
+        position: relative;
+        min-height: 100%;
+      }
+      body {
+        margin-bottom: 95px; /* Margin bottom by footer height */
+        background: #f8f9fa;
+      }
+      .container{
+        // padding-top: 25px;
+        text-align: center;
+        width: auto;
+        max-width: 680px;
+        padding: 25px 15px;
+      }
+      img {
+        max-width: 100%;
+        border-radius: 15px;
+      }
+      .input-group {
+        margin-top: 25px;
+      }
+      .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 40px; /* Set the fixed height of the footer here */
+        line-height: 40px; /* Vertically center the text there */
+        background-color: #ffffff;
+        text-align: center;
+        font-size: 10pt;
+      }
+    </style>
   </head>
   <body>
     <div class="container">
-	    <div class="row justify-content-center">
-		    <div class="col-10">
-		     <img src="{{.ImageURL}}">
-		    </div>
-		  </div>
-		  <div class="row justify-content-center">
-		    <div class="col-6">
-			    <form method="GET">
-			    	<div class="input-group input-group-lg">
-						  <input type="text" class="form-control"
-						  	name="four" value="{{.Query}}" maxlength="15">
-						  <div class="input-group-append">
-						    <button class="btn btn-outline-primary" type="submit">Create</button>
-						  </div>
-						</div>
-					</form>
-		    </div>
-		  </div>
-		</div>
-		<footer class="footer">
+      <div class="row justify-content-center">
+        <div class="col-10">
+         <img src="{{.ImageURL}}">
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <form method="GET">
+            <div class="input-group input-group-lg">
+              <input type="text" class="form-control"
+                name="four" value="{{.Query}}" maxlength="15">
+              <div class="input-group-append">
+                <button class="btn btn-outline-primary" type="submit">Create</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
        <a href="https://github.com/mhemmings/fournaan"><img height="20" width="20" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg"> Source</a> |
        <span class="text-muted">Made by <a href="https://twitter.com/mhemmings">Mark Hemmings</a> |
        Peep Show © Channel Four Television Corporation
